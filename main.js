14b8eb8a4e7f72b15278dedf9e413832c432a503
Bequa La Froth <bequa@eelfroth.com>
2018-10-28 23:43:32 +0100

    highscores, shrink over time

--- main.js
+++ main.js
@@ -1,25 +1,45 @@
 var player;
+var ball;
 var bouncies = [];
 var mouseX = window.innerWidth/2;
 var mouseY = window.innerHeight/1.5;
 var grav = 0.5;
-var score = 0;
+
+var time = 0;
+var best = 0;
+var alltime = 0;
+if (localStorage && 'alltime' in localStorage) {
+    alltime = parseFloat(localStorage.alltime);
+}
 
 var gameloop;
 
+//scoreboard
 var menu = document.createElement("menu");
-var respawn = document.createElement("menubutton");
-respawn.innerHTML = "∃  ";
-respawn.onclick = setup;
-menu.appendChild(respawn);
+var slab = document.createElement("slab");
+slab.innerHTML = "<span>∃ </span><br/>";
+slab.onclick = setup;
+var scorecurrent = document.createElement("score");
+var scorebest = document.createElement("score");
+var scorealltime = document.createElement("score");
+scorecurrent.innerHTML = time.toFixed(3);;
+scorealltime.innerHTML = alltime.toFixed(3);
+scorebest.innerHTML = best.toFixed(3);
+scorecurrent.classList.add("lb");
+scorebest.classList.add("rb");
+scorealltime.classList.add("rt");
+slab.appendChild(scorecurrent);
+slab.appendChild(scorebest);
+slab.appendChild(scorealltime);
+menu.appendChild(slab);
 document.body.appendChild(menu);
 
+//cursor position
 document.onmousemove = mousemove;
 function mousemove(e) {
     mouseX = e.pageX;
     mouseY = e.pageY;
 }
-
 document.ontouchmove = touchmove;
 function touchmove(e) {
     mouseX = e.touches[0].pageX;
@@ -29,27 +49,59 @@ function touchmove(e) {
 function setup() {
     menu.classList.add("inactive");
     document.body.style.cursor = "none";
+    scorebest.classList.remove("new");
+    scorealltime.classList.remove("new");
 
-    player = new BouncyBounce(mouseX, mouseY, 64);
-    var ball = new BouncyBounce(window.innerWidth/2, 20, 32);
+    player = new BouncyBounce(mouseX, mouseY, 80);
+    ball = new BouncyBounce(window.innerWidth/2, 20, 32);
     ball.vx = Math.random() * 6 - 3;
     bouncies.push(player);
     bouncies.push(ball);
+
+    time = 0;
 
     gameloop = setInterval(update, 16);
 }
 
 function update() {
     player.vx = (player.vx + (mouseX - player.x)*.5)/2;
-    player.vy = (player.vy + (mouseY - player.y)*.5)/2 - grav;
+    player.vy = (player.vy + (mouseY - player.y - player.r/2)*.5)/2 - grav;
+
+    //shrinking
+    player.r = Math.max(player.r - 16/1000, 16);
+    ball.r = Math.max(ball.r - 8/1000, 8);
+    player.e.style.width = player.r*2 +"px";
+    player.e.style.height = player.r*2 +"px";
+    ball.e.style.width = ball.r*2 +"px";
+    ball.e.style.height = ball.r*2 +"px";
 
     for (var i=0; i<bouncies.length; i++) {
         bouncies[i].update();
+    }
+
+    if (gameloop !== "done") {
+        time += 16/1000;
+        scorecurrent.innerHTML = time.toFixed(3);
     }
 }
 
 function gameover() {
     window.clearInterval(gameloop);
+    gameloop = "done";
+
+    //update highscores
+    if (time > best) {
+        best = time;
+        scorebest.innerHTML = best.toFixed(3);
+        scorebest.classList.add("new");
+        if (time > alltime) {
+            alltime = time;
+            scorealltime.innerHTML = alltime.toFixed(3);
+            localStorage && (localStorage.alltime = alltime);
+            scorealltime.classList.add("new");
+            scorebest.classList.remove("new");
+        }
+    }
 
     menu.classList.remove("inactive");
     document.body.style.cursor = "auto";
@@ -84,14 +136,21 @@ class BouncyBounce {
         this.vy += grav;
         this.x += this.vx;
         this.y += this.vy;
-        if (this.y-100 > window.innerHeight) {
+
+        //out of screen
+        if (this.y-100 > window.innerHeight
+         || this.y+3000 < 0
+         || this.x-100 > window.innerWidth
+         || this.x+100 < 0) {
             gameover();
         }
+
         for (var i=0; i<bouncies.length; i++) {
             if (bouncies[i] != this) {
                 this.collide(bouncies[i]);
             }
         }
+
         this.e.style.left = this.x - this.r +"px";
         this.e.style.top = this.y - this.r +"px";
     }
@@ -102,7 +161,7 @@ class BouncyBounce {
 
     collide(o) {
         var d = this.dist(this.x, this.y, o.x, o.y);
-        if (d < this.r + o.r) {
+        if (d <= this.r + o.r) {
             var overlap = (d - this.r - o.r) / 2;
             var nx = (o.x - this.x) / d;
             var ny = (o.y - this.y) / d;
